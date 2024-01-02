1ca527bfa4d42bfd65c15f37c587cf7d545d3768
Sahar Refaei <saharrefaei1999@gmail.com>
2024-01-02 17:11:56 +0100

    average

---
+++ test.py
@@ -0,0 +1,18 @@
+marks = [] 
+name=input ('name :')
+marks.append(name)
+point1 = input ('point1 : ')
+marks.append(point1)
+
+point2 = input ('point2 : ')
+marks.append(point2)
+
+point3 = input ('point3 : ')
+marks.append(point3)
+
+point4 = input ('point4 : ')
+
+marks.append(point4)
+
+print (marks)
+
