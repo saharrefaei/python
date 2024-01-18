53e96910f77af2599748d13dcefb8cc2a36db87f
Sahar Refaei <saharrefaei1999@gmail.com>
2024-01-18 10:52:49 +0100

    next prject

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-marks = []
-name = input('name: ')
-marks.append(name)
 
-point1 = float(input('point1: '))
-marks.append(point1)
-
-point2 = float(input('point2: '))
-marks.append(point2)
-
-point3 = float(input('point3: '))
-marks.append(point3)
-
-point4 = float(input('point4: '))
-marks.append(point4)
-
-average = (marks[1] + marks[2] + marks[3] + marks[4]) / 4
-print("Average:", average)
