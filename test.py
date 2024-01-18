fd3599ebbdbfcc92b8b7a350b69794dac8cd6897
Sahar Refaei <saharrefaei1999@gmail.com>
2024-01-18 11:27:16 +0100

    finding the lower number

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+N = float(input('Number ? : '))
 
+while True:
+   M = float(input("do you want more number ?"))
+   if N < M :
+     N = M 
+   s = input("do you wanna continue ?")
+   if s.lower() == "no" :
+       break
+
+print('the lower is ' , M)
