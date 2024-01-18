b87cd4242a609225cefd332bc66d2191f092a275
Sahar Refaei <saharrefaei1999@gmail.com>
2024-01-18 15:30:41 +0100

    Multiplication table

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-Number = 1
+Number = 3
 
 while Number <=10 :
     
     i=1
-    while i<=10:
+    while i>=10:
         print(i*Number ,end="\t")
         i+=1
+        continue
    
     print(Number) 
     Number+=1
